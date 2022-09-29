40d29046091aa1c1564279d2a9bd667e23fc1583
wheelercj <chrisw289@gmail.com>
2022-09-29 12:01:41 -0700

    Move the validators to their own file

--- src/moviefinder/account_creation_menu.py
+++ src/moviefinder/account_creation_menu.py
@@ -1,26 +1,7 @@
-import re
-
-from PySide6 import QtGui
+from moviefinder.validators import EmailValidator
+from moviefinder.validators import PasswordValidator
 from PySide6 import QtWidgets
 from PySide6.QtCore import Qt
-
-
-class EmailValidator(QtGui.QValidator):
-    email_pattern = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
-
-    def validate(self, email: str, cursor_position: int) -> QtGui.QValidator.State:
-        # TODO: check if an account was already made with the given email address.
-        # return QtGui.QValidator.Invalid
-        if self.email_pattern.match(email):
-            return QtGui.QValidator.Acceptable
-        return QtGui.QValidator.Intermediate
-
-
-class PasswordValidator(QtGui.QValidator):
-    def validate(self, password: str, cursor_position: int) -> QtGui.QValidator.State:
-        if 9 <= len(password) <= 50:
-            return QtGui.QValidator.Acceptable
-        return QtGui.QValidator.Intermediate
 
 
 class AccountCreationMenu(QtWidgets.QWidget):
