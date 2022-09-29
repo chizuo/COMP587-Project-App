6cb0ca0ce3c95046582bab4b13ba7933579515a8
wheelercj <chrisw289@gmail.com>
2022-09-29 10:35:03 -0700

    Create a simple email validator that only uses regex

--- src/moviefinder/account_creation_menu.py
+++ src/moviefinder/account_creation_menu.py
@@ -1,5 +1,19 @@
+import re
+
+from PySide6 import QtGui
 from PySide6 import QtWidgets
 from PySide6.QtCore import Qt
+
+
+class EmailValidator(QtGui.QValidator):
+    email_pattern = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+
+    def validate(self, email: str, cursor_position: int) -> QtGui.QValidator.State:
+        # TODO: check if an account was already made with the given email address.
+        # return QtGui.QValidator.Invalid
+        if self.email_pattern.match(email):
+            return QtGui.QValidator.Acceptable
+        return QtGui.QValidator.Intermediate
 
 
 class AccountCreationMenu(QtWidgets.QWidget):
@@ -12,6 +26,7 @@ class AccountCreationMenu(QtWidgets.QWidget):
         self.name_line_edit = QtWidgets.QLineEdit(self)
         self.layout.addRow("name:", self.name_line_edit)
         self.email_line_edit = QtWidgets.QLineEdit(self)
+        self.email_line_edit.setValidator(EmailValidator())
         self.layout.addRow("email:", self.email_line_edit)
         self.password_line_edit = QtWidgets.QLineEdit(self)
         self.password_line_edit.setEchoMode(QtWidgets.QLineEdit.Password)
