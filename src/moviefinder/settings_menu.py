8b21680ba95096bd4ab549eccbfd1cfeb230fbac
wheelercj <chrisw289@gmail.com>
2022-12-26 18:37:47 -0800

    Add the options button and genre selector to the settings menu

--- src/moviefinder/settings_menu.py
+++ src/moviefinder/settings_menu.py
@@ -1,4 +1,5 @@
 from moviefinder.buttons import add_services_groupbox
+from moviefinder.checkable_combo_box import CheckableComboBox
 from moviefinder.country_code import CountryCode
 from moviefinder.movie import ServiceName
 from moviefinder.movies import movies
@@ -15,24 +16,31 @@ class SettingsMenu(QtWidgets.QWidget):
     def __init__(self, main_window: QtWidgets.QMainWindow):
         super().__init__(main_window)
         self.main_window = main_window
-        self.layout = QtWidgets.QFormLayout(self)
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.options_button = main_window.create_options_button(self)
+        self.layout.addWidget(self.options_button, alignment=Qt.AlignRight)
+        settings_layout = QtWidgets.QFormLayout(self)
         title_label = QtWidgets.QLabel("<h1>settings</h1>", self)
         title_label.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
-        self.layout.addRow(title_label)
-        self.layout.addRow("email:", QtWidgets.QLabel(user.email, self))
+        settings_layout.addRow(title_label)
+        settings_layout.addRow("email:", QtWidgets.QLabel(user.email, self))
         self.name_line_edit = QtWidgets.QLineEdit(self)
         self.name_line_edit.setValidator(NameValidator())
-        self.layout.addRow("name:", self.name_line_edit)
+        settings_layout.addRow("name:", self.name_line_edit)
         self.password_line_edit = QtWidgets.QLineEdit(self)
         self.password_line_edit.setEchoMode(QtWidgets.QLineEdit.Password)
         self.password_line_edit.setValidator(PasswordValidator())
-        self.layout.addRow("new password:", self.password_line_edit)
+        settings_layout.addRow("new password:", self.password_line_edit)
         self.confirm_password_line_edit = QtWidgets.QLineEdit(self)
         self.confirm_password_line_edit.setEchoMode(QtWidgets.QLineEdit.Password)
-        self.layout.addRow("confirm new password:", self.confirm_password_line_edit)
+        settings_layout.addRow("confirm new password:", self.confirm_password_line_edit)
+        self.genres_combo_box = CheckableComboBox(self)
+        self.genres_combo_box.addItems(user.genre_habits.keys())
+        self.genres_combo_box.setCurrentData(movies.genres)
+        settings_layout.addRow("genres:", self.genres_combo_box)
         self.region_combo_box = QtWidgets.QComboBox(self)
         self.region_combo_box.addItem(CountryCode.US.value)
-        self.layout.addRow("region:", self.region_combo_box)
+        settings_layout.addRow("region:", self.region_combo_box)
         add_services_groupbox(self)
         buttons_layout = QtWidgets.QHBoxLayout()
         self.save_button = QtWidgets.QPushButton("save", self)
@@ -41,13 +49,14 @@ class SettingsMenu(QtWidgets.QWidget):
         self.cancel_button = QtWidgets.QPushButton("cancel", self)
         self.cancel_button.clicked.connect(self.__reset_settings_and_show_browse_menu)
         buttons_layout.addWidget(self.cancel_button)
-        self.layout.addRow(buttons_layout)
+        settings_layout.addRow(buttons_layout)
         self.__set_widgets()
 
     def __set_widgets(self) -> None:
         """Initializes widgets with the values in the user object."""
         self.name_line_edit.setText(user.name)
         assert user.region is not None
+        self.genres_combo_box.setCurrentData(movies.genres)
         self.region_combo_box.setCurrentText(user.region.value)
         self.amazon_prime_checkbox.setChecked(ServiceName.AMAZON_PRIME in user.services)
         self.apple_tv_plus_checkbox.setChecked(
@@ -92,6 +101,10 @@ class SettingsMenu(QtWidgets.QWidget):
             self.confirm_password_line_edit.clear()
             show_message_box("The passwords do not match.")
             return
+        new_genres = self.genres_combo_box.currentText().split(", ")
+        if not new_genres:
+            show_message_box("Please choose at least one genre.")
+            return
         if not valid_services_groupbox(self.services_group_box):
             show_message_box("Please choose at least one service.")
             return
@@ -102,7 +115,12 @@ class SettingsMenu(QtWidgets.QWidget):
         new_region = CountryCode(self.region_combo_box.currentText())
         new_services: list[ServiceName] = self.__get_services()
         must_reload_movies = False
-        if new_region != user.region or new_services != user.services:
+        if (
+            new_genres != movies.genres
+            or new_region != user.region
+            or new_services != user.services
+        ):
+            movies.genres = new_genres
             must_reload_movies = True
         if not user.update_and_save(new_name, new_region, new_services, new_password):
             user.clear()
